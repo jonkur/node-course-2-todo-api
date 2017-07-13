df1c6e08a5d0d135d024885d7e4bc7ebfb0d9414
jonkur <jonkur@users.noreply.github.com>
2017-07-14 01:29:53 +0300

    yet another mLab related commit

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/TodoApp', {useMongoClient: true});
-mongoose.connect('mongodb://jkur2:asdewq123@ds159112.mlab.com:59112/todoapi-db', {useMongoClient: true}); // mlab test database pw: 2dfwaw3f43we
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {useMongoClient: true}); // mlab test database pw: 2dfwaw3f43we
 
 module.exports = { mongoose };
