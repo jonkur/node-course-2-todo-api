2d21c3cffb306976d300bf7fee2e7962a0567364
jonkur <jonkur@users.noreply.github.com>
2017-07-24 18:07:02 +0300

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -55,6 +55,18 @@ UserSchema.methods.generateAuthToken = function() { // Using a normal function i
 	} );
 };
 
+UserSchema.methods.removeToken = function (token) {
+	var user = this;
+
+	return user.update({
+		$pull: {
+			tokens: {
+				token: token
+			}
+		}
+	});
+};
+
 UserSchema.statics.findByToken = function(token) {
 	var User = this;
 	var decoded;
