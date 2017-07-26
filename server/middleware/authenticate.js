8980e9d5394bcc7c40f4575f32135261813b67c6
jonkur <jonkur@users.noreply.github.com>
2017-07-26 18:12:15 +0300

    Quick fix for authentication error

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -2,6 +2,7 @@ var {User} = require('./../models/user');
 
 var authenticate = (req, res, next) => {
 	var token = req.header('x-auth');
+	if (!token) res.status(401).send();
 
 	User.findByToken(token).then( (user) => {
 		if (!user) {
